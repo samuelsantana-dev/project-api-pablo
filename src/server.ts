6364dc10e61df23ee0a814c351c88d99fa2f9217
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-03-21 09:27:25 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { userRoutes } from './routes/userRoutes'
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/users', userRoutes);
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
